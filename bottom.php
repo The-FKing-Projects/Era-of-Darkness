6d26421d3a454ec3afb812e8d0e80cb2d5bd708c
Dennis Dodd <FiendKing04@yahoo.com>
2015-01-21 10:56:39 -0500

    Changing License
    Changed the bottom.php license from GNU GPL to MIT.

--- bottom.php
+++ bottom.php
@@ -6,4 +6,4 @@
 	<a href="spam.php">Report Spam</a> | 
 	<a href="tos.php">Terms of Service</a> | 
 	<a href="store.php">Store</a>
-	<br>Created with love and all source code released freely on <a href="https://github.com/TheAugdenGroup/EraofDarkness" title="Please fork us!">GitHub</a> via the <a href="https://www.gnu.org/licenses/gpl.html">GNU GPL</a> by <a href=https://augden.org/ title="AKA 'FiendKing04' and 'Rasputin'">The Augden Group</br></font></center>
+	<br>Created with love and all source code released freely on <a href="https://github.com/TheAugdenGroup/EraofDarkness" title="Please fork us!">GitHub</a> via the <a href="http://opensource.org/licenses/MIT">MIT License</a> by <a href=https://augden.org/ title="AKA 'FiendKing04' and 'Rasputin'">The Augden Group</br></font></center>
