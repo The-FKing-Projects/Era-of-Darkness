1078eda5d7c18321a9618dafe4831b2703ec96de
Dennis Dodd <FiendKing04@yahoo.com>
2015-01-21 10:49:15 -0500

    Index, Bottom, Left and Top
    Adding the bottom.php, left,php and top.php, and updated the index.php.

---
+++ bottom.php
@@ -0,0 +1,9 @@
+<center>
+    <font style="font-size: 12pt">
+	<a href="contact.php">Advertising & Business Inquiries</a> | 
+	<a href="contact.php">Contact Us</a> | 
+	<a href="privacy.php">Privacy Policy</a> | 
+	<a href="spam.php">Report Spam</a> | 
+	<a href="tos.php">Terms of Service</a> | 
+	<a href="store.php">Store</a>
+	<br>Created with love and all source code released freely on <a href="https://github.com/TheAugdenGroup/EraofDarkness" title="Please fork us!">GitHub</a> via the <a href="https://www.gnu.org/licenses/gpl.html">GNU GPL</a> by <a href=https://augden.org/ title="AKA 'FiendKing04' and 'Rasputin'">The Augden Group</br></font></center>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html><head><title>Era of Darkness</title>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<link href="css/common.css" type=text/css rel=stylesheet>
+<link href="css/main.css" type=text/css rel=stylesheet>
+<META content="eraofdarkness, mmo, mmorpg" name=keywords>
+<META content="Era of Darkness is a massively multiplayer online role playing game. Players choose a nation and faction and build armies, recruit friends as officers, buy weapons, and spy on and attack each other." name=description>
+<META content="Era of Darkness" name=author></head>
+
+<BODY text=#000000 bgColor=#ffffff leftMargin=0 topMargin=0 marginheight="0" 
+marginwidth="0">
+
+<?
+	include ("top.php");
+?>
+<?
+	include ("left.php");
+?>
+<?
+	  include ("bottom.php");
+?>
+</body></html>
