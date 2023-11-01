6eddb1909ffbffe8566783935a4208bcdf78752e
Dennis Dodd <FiendKing04@yahoo.com>
2023-10-31 20:41:07 -0400

    Switched to light theme. Removed Google AdSense until further notice.

--- index.php
+++ index.php
@@ -7,16 +7,8 @@
 <META content="Era of Darkness is a massively multiplayer online role playing game. Players choose a nation and faction and build armies, recruit friends as officers, buy weapons, and spy on and attack each other." name=description>
 <META content="Era of Darkness" name=author></head>
 
-<body text="#FF0033" bgcolor="#000000" link="#FF0033' vlink="#FF0033" alink="#FF0033">
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Era of Darkness -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-9709758420986273"
-     data-ad-slot="2273217943"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<body text="#FF0033" bgcolor="#FFFFFF" link="#FF0033' vlink="#FF0033" alink="#FF0033">
+
 <?
 	include ("top.php");
 ?>
