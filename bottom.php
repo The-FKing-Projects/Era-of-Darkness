cce297f7282a337cdc8639bd470fe92b62a3dd59
Dennis Dodd <FiendKing04@yahoo.com>
2023-10-31 20:29:59 -0400

    Fixed spacing.

--- bottom.php
+++ bottom.php
@@ -5,7 +5,7 @@
 	<a href="contact.php">Contact Us</a> | 
 	<a href="privacy.php">Privacy Policy</a> | 
 	<a href="spam.php">Report Spam</a> | 
-	<a href="store.php">Store</a>
+	<a href="store.php">Store</a> |
 	<a href="tos.php">Terms of Service</a> | 
 	<br>Created with love by <a href="https://fkingprojects.org/">The FKing's Projects</a> <a href="team.php">team</a>.</br>
 	<br>All source code released freely on <a href="https://github.com/The-FKing-Projects/Era-of-Darkness" title="Please fork us!">GitHub</a> under the <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU GPL v3</a></br></font></center>
