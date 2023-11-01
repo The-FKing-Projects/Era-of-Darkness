2c293172209732c8e4ca07212ab805bea0cadf44
Dennis Dodd <FiendKing04@yahoo.com>
2023-10-31 20:28:04 -0400

    Fixed hyperlink.

--- bottom.php
+++ bottom.php
@@ -7,5 +7,5 @@
 	<a href="spam.php">Report Spam</a> | 
 	<a href="store.php">Store</a>
 	<a href="tos.php">Terms of Service</a> | 
-	<br>Created with love by <a href="https://fkingprojects.org/>The FKing's Projects</a> <a href="team.php">team</a>.</br>
+	<br>Created with love by <a href="https://fkingprojects.org/">The FKing's Projects</a> <a href="team.php">team</a>.</br>
 	<br>All source code released freely on <a href="https://github.com/The-FKing-Projects/Era-of-Darkness" title="Please fork us!">GitHub</a> under the <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU GPL v3</a></br></font></center>
